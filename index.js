58592609804f1fe376a2f5a82336bc558928e3d2
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-24 23:04:06 +0200

    Final touches

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const senecaIndex = require('./lib/seneca-index')
 const senecaData = require('./lib/seneca-data')
 const senecaParse = require('./lib/seneca-parse')
 const senecaCounter = require('./lib/seneca-counter')
+const senecaQueue = require('./lib/seneca-queue')
+const senecaGaKill = require('./lib/seneca-ga-kill')
 
 seneca.use(senecaIndex)
 seneca.use(senecaData)
 seneca.use(senecaParse)
 seneca.use(senecaCounter)
+seneca.use(senecaQueue)
+seneca.use(senecaGaKill)
 
 seneca.listen()
 
