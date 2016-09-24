4ede7a3d6368aec656e814f75bab950625bc46c4
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-24 12:47:13 +0200

    Adds parse

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 
 const seneca = require('seneca')()
 const senecaIndex = require('./lib/seneca-index')
+const senecaData = require('./lib/seneca-data')
+const senecaParse = require('./lib/seneca-parse')
 
 seneca.use(senecaIndex)
+seneca.use(senecaData)
+seneca.use(senecaParse)
+
+seneca.listen()
 
 seneca.on('ready', () => {
   seneca.act('role:index, cmd:delete', (error, msg) => {
     if (error) {
       throw error
     } else {
-      seneca.act('role:data, cmd:collect')
+      seneca.act('role:data, cmd:collect, type:meetings')
+      seneca.act('role:data, cmd:collect, type:politicians')
+      seneca.act('role:data, cmd:collect, type:committees')
+      seneca.act('role:data, cmd:collect, type:parties')
     }
   })
 })
---
+++ test/modules/wreck-test.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const tap = require('tap')
+const Wreck = require('wreck')
+
+tap.ok(Wreck, 'Wreck loads OK')
